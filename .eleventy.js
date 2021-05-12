f948936e7d4d411c36ed7e8ceec1783a1979206b
Kharleann Cabrera <kharleann.cabrera@eyss.io>
2021-05-12 18:38:05 -0400

    cssmin

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+const CleanCSS = require("clean-css");
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addFilter("cssmin", function (code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
+	eleventyConfig.addPassthroughCopy("images");
+	eleventyConfig.addPassthroughCopy("admin");
+	eleventyConfig.addPassthroughCopy("css");
+	eleventyConfig.addPassthroughCopy("js");
+};
